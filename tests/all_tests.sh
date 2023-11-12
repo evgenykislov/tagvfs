8d972410c4a4f5813e4fb23c84b27cdf09317981
Evgeny Kislov <dev@evgenykislov.com>
2023-11-12 18:54:40 +0300

    Add commands for kernel memory leak detection.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+if $(sudo test -e /sys/kernel/debug/kmemleak); then
+  echo clear | sudo tee /sys/kernel/debug/kmemleak
+fi
+
 . ./clear.sh
 . ./compile_run.sh
 
@@ -7,8 +12,9 @@
 . ./test_same_filename_copy.sh
 
 
-
-
-
-
 . ./stop.sh
+
+if $(sudo test -e /sys/kernel/debug/kmemleak); then
+  echo scan | sudo tee /sys/kernel/debug/kmemleak
+  sudo cat /sys/kernel/debug/kmemleak > memleak.txt
+fi
--- tests/compile_run.sh
+++ tests/compile_run.sh
@@ -18,3 +18,14 @@ if [[ $(lsmod | grep tagvfs) ]]; then
 fi
 
 sudo insmod ./${TESTDIR}/tagvfs.ko
+
+# Get debug information in case of memleak detection
+if $(sudo test -f /sys/kernel/debug/kmemleak); then
+  BASEPATH=$(pwd)
+  pushd /sys/module/tagvfs/sections
+  echo ".text .data .bss" > ${BASEPATH}/sections.txt
+  sudo cat .text .data .bss >> ${BASEPATH}/sections.txt
+  popd
+
+  objdump -DS ./${TESTDIR}/tagvfs.ko > disasm.txt
+fi
