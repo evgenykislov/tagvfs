03c79e1275a943b98f887f78a70884c2c250e3fb
Evgeny Kislov <dev@evgenykislov.com>
2023-12-17 11:43:54 +0300

    Update test script of base functions.

--- tests/test_base_functions.sh
+++ tests/test_base_functions.sh
@@ -1,5 +1,58 @@
 #!/bin/bash
 
+
+function CheckTag1Exist() {
+  EXIST_TAG1="0"
+  for f in ${TAGDIR}/* ; do
+    if [[ "$(basename $f)" == "tag1" ]]; then
+      EXIST_TAG1="1"
+    fi
+  done
+
+  if ! [[ "${EXIST_TAG1}" == "1" ]]; then
+    echo "ERROR: BASE FUNCTIONS: Tag tag1 is created but it hasn't listed in directory"
+    exit 1
+  fi
+
+  EXIST_TAG1="0"
+  for f in ${RPATH}/only-tags/* ; do
+    if [[ $(basename $f) == "tag1" ]]; then
+      EXIST_TAG1="1"
+    fi
+  done
+
+  if ! [[ "${EXIST_TAG1}" == "1" ]]; then
+    echo "ERROR: BASE FUNCTIONS: Tag tag1 is created but it hasn't listed in only-tags directory"
+    exit 1
+  fi
+}
+
+
+function CheckBF1Exist() {
+  if ! [[ -L "${TAGDIR}/base_functions_001" ]]; then
+    echo "ERROR: BASE FUNCTIONS: file base_functions_001 doesn't exist"
+    exit 1
+  fi
+
+  if ! [[ "$(readlink -f "${TAGDIR}/base_functions_001")" == "${TPATH}/base_functions_001" ]] ; then
+    echo "ERROR: BASE FUNCTIONS: Wrong target for base_functions_001"
+    exit 1
+  fi
+
+  if ! [[ -L "${TAGDIR}/no-tag1/base_functions_001" ]]; then
+    echo "ERROR: BASE FUNCTIONS: file no-tag1/base_functions_001 doesn't exist"
+    exit 1
+  fi
+
+  if ! [[ "$(readlink -f "${TAGDIR}/no-tag1/base_functions_001")" == "${TPATH}/base_functions_001" ]] ; then
+    echo "ERROR: BASE FUNCTIONS: Wrong target for no-tag1/base_functions_001"
+    exit 1
+  fi
+}
+
+
+# ------------------
+# ------------------
 if [[ ${TESTDIR} == "" ]]; then
   echo "ERROR: WRONG CONTEXT"
   exit 1
@@ -26,7 +79,6 @@ fi
 mkdir base_functions
 
 
-
 sudo mount -t tagvfs $(pwd)/base_functions.tag $(pwd)/base_functions/
 # Path to tag directory
 TAGDIR=${RPATH}/tags
@@ -42,29 +94,8 @@ if [[ $(mkdir ${TAGDIR}/tag1 > /dev/null 2> /dev/null) ]]; then
 fi
 set -o errexit
 
-EXIST_TAG1="0"
-for f in ${TAGDIR}/* ; do
-  if [[ "$(basename $f)" == "tag1" ]]; then
-    EXIST_TAG1="1"
-  fi
-done
+CheckTag1Exist
 
-if ! [[ "${EXIST_TAG1}" == "1" ]]; then
-  echo "ERROR: BASE FUNCTIONS: Tag tag1 is created but it hasn't listed in directory"
-  exit 1
-fi
-
-EXIST_TAG1="0"
-for f in ${RPATH}/only-tags/* ; do
-  if [[ $(basename $f) == "tag1" ]]; then
-    EXIST_TAG1="1"
-  fi
-done
-
-if ! [[ "${EXIST_TAG1}" == "1" ]]; then
-  echo "ERROR: BASE FUNCTIONS: Tag tag1 is created but it hasn't listed in only-tags directory"
-  exit 1
-fi
 
 # Check filelink creation and exstance into no-tags
 # -----
@@ -78,25 +109,13 @@ if [[ $(ln -s --target-directory="${TAGDIR}" "${TPATH}/base_functions_001" > /de
 fi
 set -o errexit
 
-if ! [[ -L "${TAGDIR}/base_functions_001" ]]; then
-  echo "ERROR: BASE FUNCTIONS: file base_functions_001 doesn't exist"
-  exit 1
-fi
+CheckBF1Exist
 
-if ! [[ "$(readlink -f "${TAGDIR}/base_functions_001")" == "${TPATH}/base_functions_001" ]] ; then
-  echo "ERROR: BASE FUNCTIONS: Wrong target for base_functions_001"
-  exit 1
-fi
-
-if ! [[ -L "${TAGDIR}/no-tag1/base_functions_001" ]]; then
-  echo "ERROR: BASE FUNCTIONS: file no-tag1/base_functions_001 doesn't exist"
-  exit 1
-fi
-
-if ! [[ "$(readlink -f "${TAGDIR}/no-tag1/base_functions_001")" == "${TPATH}/base_functions_001" ]] ; then
-  echo "ERROR: BASE FUNCTIONS: Wrong target for no-tag1/base_functions_001"
-  exit 1
-fi
+# stop, start, repeat checks
+sudo umount $(pwd)/base_functions/
+sudo mount -t tagvfs $(pwd)/base_functions.tag $(pwd)/base_functions/
+CheckTag1Exist
+CheckBF1Exist
 
 echo "--- OK: BASE FUNCTIONS ---"
 
